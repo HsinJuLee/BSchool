7eec267563143c4d2d8c340c92a7a61a401cb2ac
Hsin Ju Lee <leehsinju@gmail.com>
2015-05-30 16:13:15 +0100

    Setup basic config file

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import os
+
+print(os.environ['APP_SETTINGS'])
+
 app = Flask(__name__)
+app.config.from_objects(os.environ['APP_SETTINGS'])
+
 
 @app.route("/")
 def hello():
     return "Schools and houses go here!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
